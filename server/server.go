9e3d44ea23096057f2d00db7612fa4fe7df37c24
oasis future <oa@edzdeMacBook-Pro.local>
2019-02-22 16:15:21 +0800

    update server and client

--- server/server.go
+++ server/server.go
@@ -6,12 +6,47 @@ import (
 	"net"
 )
 
-func Start() {
+const (
+	addr     = "0.0.0.0"
+	port     = "8080"
+	buffSize = 1024
+)
 
-	addr := "0.0.0.0:8080"
+type Actor struct {
+}
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
+func (a *Actor) resp(req string) string {
+	switch req {
+	case "hello":
+	case "hi":
+		return "hi, i'm server. "
+	default:
+		return "i don't quit understand. "
+	}
 
+	return "something wrong. "
+}
+
+type Server struct {
+	listenAddr string
+	port       string
+
+	pool  Pool
+	actor Actor
+}
+
+type Pool struct {
+	cs []net.Conn
+}
+
+func (p *Pool) add(c net.Conn) {
+}
+
+func (s *Server) Start() {
+	s.listenAddr = addr
+	s.port = port
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", addr+":"+port)
 	if err != nil {
 		log.Fatalf("net.ResovleTCPAddr fail:%s", addr)
 	}
@@ -31,21 +66,56 @@ func Start() {
 			continue
 		}
 
-		go handleConnection(conn)
-
+		go s.handleConnection(conn)
 	}
-
 }
 
-func handleConnection(conn net.Conn) {
+func (s *Server) handleConnection(c net.Conn) {
+	buff := make([]byte, 0, buffSize)
 
+	defer func() {
+		err := c.Close()
+		if err != nil {
+			fmt.Println("connection close error. ")
+		}
+	}()
+
+	// return hello
 	var buffer = []byte("You are welcome. I'm server.")
 
-	n, err := conn.Write(buffer)
+	n, err := c.Write(buffer)
 
 	if err != nil {
 		fmt.Println("Write error:", err)
 	}
 
 	fmt.Println("send bytes:", n)
+
+	for {
+		n, err := c.Read(buff)
+		if err != nil {
+			fmt.Println("error:", err.Error())
+			return
+		}
+
+		fmt.Println(n, " bytes received. ")
+
+		content := string(buff)
+		resp := s.actor.resp(content)
+
+		fmt.Println("received msg :", content)
+
+		n, err = c.Write([]byte(resp))
+		if err != nil {
+			fmt.Println("error:", err.Error())
+			return
+		}
+
+		fmt.Println("write success. ")
+	}
+}
+
+func Start() {
+	s := Server{}
+	s.Start()
 }
